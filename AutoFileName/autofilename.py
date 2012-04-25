4c9225583cd3b945fab22f10b4d14ed3e49c2f83
Andreas Wahlqvist <andreas@deepfriedmind.com>
2012-04-24 23:42:29 -0400

    update + removed some unused packages

--- AutoFileName/autofilename.py
+++ AutoFileName/autofilename.py
@@ -6,27 +6,28 @@ class FileNameComplete(sublime_plugin.EventListener):
         sel = view.sel()[0].a
         return string in view.substr(sublime.Region(sel-1, sel))
 
+    def is_empty_string(self,view,sel):
+        string = view.substr(view.extract_scope(sel))
+        return len(string) < 3
+
     def on_query_completions(self, view, prefix, locations):
         completions = []
+        valid_scopes = ["string", "css", "sass", "scss"]
+        backup = []
         sel = view.sel()[0].a
 
-        if self.prev_has(view, '/') or self.prev_has(view, '\\\\'):
-            if "string" in view.scope_name(sel):
-                pass
-            elif "css" in view.scope_name(sel):
-                pass
-            elif "sass" in view.scope_name(sel):
-                pass
-            else:
-                return []
-        else:
-            return []
+        for x in view.find_all("[a-zA-Z]+"):
+                    backup.append(view.substr(x))
+
+        if not any(s in view.scope_name(sel) for s in valid_scopes):
+            return backup
+        if not (self.is_empty_string(view,sel) or self.prev_has(view, '/') or self.prev_has(view, '\\\\')):
+            return backup
 
         this_dir = os.path.split(view.file_name())[0] + os.path.sep
-
         cur_path = view.substr(view.extract_scope(sel-1)).replace('\r\n', '\n').split('\n')[0]
 
-        if cur_path.startswith(("'","\"")):
+        if cur_path.startswith(("'","\"","(")):
             cur_path = cur_path[1:-1]
 
         this_dir = os.path.join(this_dir, cur_path)
@@ -40,4 +41,4 @@ class FileNameComplete(sublime_plugin.EventListener):
             return [(x, x) for x in list(set(completions))]
         except OSError:
             print "AutoFileName: could not find " + this_dir
-            return []
+            return backup
