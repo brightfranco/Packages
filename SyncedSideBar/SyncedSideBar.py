946b50f579a44956d26bdb93f049e3f0d0847cd6
Andreas Wahlqvist <andreas@deepfriedmind.com>
2012-10-11 17:04:52 +0200

    update

--- SyncedSideBar/SyncedSideBar.py
+++ SyncedSideBar/SyncedSideBar.py
@@ -1,14 +1,9 @@
-import sublime
 import sublime_plugin
-
-
-def get_settings_value(key):
-    settings = sublime.load_settings(__name__ + '.sublime-settings')
-    return settings.get(key)
 
 
 class SideBarListener(sublime_plugin.EventListener):
 
     def on_activated(self, view):
-        if get_settings_value('reveal-on-activate'):
-            view.window().run_command('reveal_in_side_bar')
+        active = view.window()
+        if active and view.settings().get('reveal-on-activate') != False:
+            active.run_command('reveal_in_side_bar')
