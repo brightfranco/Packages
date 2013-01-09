9a70330a171163a1c974e32d628d1764346d0513
Andreas Wahlqvist <andreas@deepfriedmind.com>
2013-01-09 10:11:55 +0100

    update

--- GitGutter/git_gutter_events.py
+++ GitGutter/git_gutter_events.py
@@ -2,17 +2,17 @@ import sublime
 import sublime_plugin
 from view_collection import ViewCollection
 
+
 class GitGutterEvents(sublime_plugin.EventListener):
+    def on_modified(self, view):
+        if view.settings().get('git_gutter_live_mode', True):
+            ViewCollection.add(view)
 
-  def on_load(self, view):
-    ViewCollection.add(view)
+    def on_clone(self, view):
+        ViewCollection.add(view)
 
-  def on_modified(self, view):
-    if view.settings().get('git_gutter_live_mode', True):
-      ViewCollection.add(view)
+    def on_post_save(self, view):
+        ViewCollection.add(view)
 
-  def on_clone(self, view):
-    ViewCollection.add(view)
-
-  def on_post_save(self, view):
-    ViewCollection.add(view)
+    def on_activated(self, view):
+        ViewCollection.add(view)
